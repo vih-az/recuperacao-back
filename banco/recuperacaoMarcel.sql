ff880c6cb5412ae33b354e02434e6a5bf01402f1
vih-az <124360742+vih-az@users.noreply.github.com>
2024-06-18 01:15:02 -0300

    Update recuperacaoMarcel.sql

--- banco/recuperacaoMarcel.sql
+++ banco/recuperacaoMarcel.sql
@@ -8,3 +8,16 @@ create table tarefas(
      dataFim date,
      statusT varchar(10) not null
 );
+create table usuario(
+     idUsuario int not null auto_increment primary key,
+     nomeUsuario varchar(200) not null,
+     emailUsuarios varchar(20) not null,
+     senhaUsuario varchar(10) not null
+);
+create table tarefasUsuario(
+     idTarefasUsuario int not null auto_increment primary key,
+     tarefasId int,
+     usuarioId int,
+     foreign key(tarefasId) references tarefas(idTarefas),
+     foreign key(usuarioId) references usuario(idUsuario)
+);
